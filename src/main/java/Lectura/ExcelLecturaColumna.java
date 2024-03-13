3a399ce320a973544f11fdf72d882a64917ccf57
Andres Uribe G. <neluri@comfenalcoquindio.co>
2024-03-13 17:58:16 -0500

    Lectura de filas

--- src/main/java/Lectura/ExcelLecturaColumna.java
+++ src/main/java/Lectura/ExcelLecturaColumna.java
@@ -7,18 +7,17 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Iterator;
 
-public class ExcelLectura {
+public class ExcelLecturaColumna {
     public static void main(String[] args) {
         File archivo = new File("Datos.xlsx");
         try {
             InputStream input = new FileInputStream(archivo);
             XSSFWorkbook libro = new XSSFWorkbook(input);
             XSSFSheet hoja = libro.getSheetAt(0);
-//            Row fila = hoja.getRow(1) asi traería la fila específica
+            /*Row fila = hoja.getRow(1) asi traería la fila específica*/
             Iterator<Row> filas = hoja.rowIterator();
 
             Cell columna =null;
--- src/main/java/Lectura/ExcelLecturaFila.java
+++ src/main/java/Lectura/ExcelLecturaFila.java
@@ -1,2 +1,45 @@
-package Lectura;public class ExcelLecturaFila {
+package Lectura;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Iterator;
+
+public class ExcelLecturaFila {
+    public static void main(String[] args) {
+        File archivo = new File("C:/Proyectos/ApachePoiExcel/Files/Datos.xlsx");
+        try {
+            InputStream input = new FileInputStream(archivo);
+            XSSFWorkbook libro = new XSSFWorkbook(input);
+            XSSFSheet hoja = libro.getSheetAt(1);
+            /* XSSFSheet hoja = libro.getSheet("Fila");  Se puede tambien indicar el nombre de la hoja*/
+        Row fila = hoja.getRow(1);
+            Iterator<Cell> columnas = fila.cellIterator();
+            while (columnas.hasNext()){
+                Cell celda = columnas.next();
+                if (celda.getCellType()== CellType.STRING) {
+                    String valor = celda.getStringCellValue();
+                    System.out.println(valor);
+                }
+                if (celda.getCellType()== CellType.NUMERIC){
+                    double valor = celda.getNumericCellValue();
+                    System.out.println(valor);
+                }
+                if (celda.getCellType()==CellType.NUMERIC && DateUtil.isCellDateFormatted(celda)){
+                    Date fecha = celda.getDateCellValue();
+                    System.out.println(fecha);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
